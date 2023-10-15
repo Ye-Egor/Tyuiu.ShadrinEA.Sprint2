12866f09886937480c7f881e17d3f9cc48f5bd3d
Ye-Egor <Shadrin-yegor29@yandex.ru>
2023-10-15 23:38:39 +0500

    .

--- Tyuiu.ShadrinEA.Sprint2.Task1.V6/Program.cs
+++ Tyuiu.ShadrinEA.Sprint2.Task1.V6/Program.cs
@@ -13,6 +13,46 @@ namespace Tyuiu.ShadrinEA.Sprint2.Task1.V6
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Шадрин Е.А | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы линейной структуры                                      *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Шадрин Егор Андреевич | ИСТНб-23-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+          
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+
+            Console.WriteLine("Введите значение A:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение B:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение C:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение D:");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"{ ds.GetLogicOperations( a, b, c, d)}");
+            Console.ReadKey();
         }
     }
 }
